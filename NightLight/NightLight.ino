557f79165ad860bba7c2568ea37c5b0374de361f
allitode <allitode@gmail.com>
2018-04-14 13:02:54 -0500

    Add project files.

---
+++ NightLight/NightLight.ino
@@ -0,0 +1,17 @@
+/*
+ Name:		NightLight.ino
+ Created:	4/14/2018 1:01:26 PM
+ Author:	Jace
+*/
+
+#include <Adafruit_NeoPixel.h>
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
